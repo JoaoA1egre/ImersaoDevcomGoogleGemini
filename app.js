0bdfc08ffdf36b1db3308363bbec27c38ac93eec
JoaoA1egre <guivogelsanguer@gmail.com>
2024-09-10 20:57:44 -0300

    Inserção de HTML pelo JavaScript

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+let section = document.getElementById("resultados-pesquisa");
+console.log(section);
+
+let resultados = "";
+
+for (let dado of dados) {
+    resultados += `
+    <div class="item-resultado">
+        <h2>${dado.titulo}</h2>
+        <p class="descricao-meta">${dado.descricao}</p>
+        <a href=${dado.link} target="_blank">Mais informações</a>
+    </div>
+    `
+};
+
+section.innerHTML = resultados;
---
+++ dados.js
@@ -0,0 +1,31 @@
+let dados = [
+{
+    titulo: "Pastilha de Freio",
+    descricao: "Pastilha de freio dianteira",
+    link: "https://1229778.commercesuite.com.br/pastilhas-de-freio/ford/new-fiesta-new-ka-new-suzuki-swift",
+},
+{
+    titulo: "Pastilha de Freio",
+    descricao: "Pastilha de freio traseira",
+    link: "https://www.motobr.com.br/pastilha-de-freio",
+},
+{
+    titulo: "Óleo de Motor",
+    descricao: "Óleo sintético 5W-30",
+    link: "https://www.petrobras.com.br/produtos/lubrificantes/oleos-motores"
+},
+{
+    titulo: "Óleo do cambio",
+    descricao: "Óleo sintético 5W-30",
+    link: "https://www.petrobras.com.br/produtos/lubrificantes/oleos-motores"
+},
+{
+    titulo: "Filtro de Ar",
+    descricao: "Filtro de ar esportivo",
+    link: "https://www.lojadoauto.com.br/filtros"
+},
+{
+    titulo: "Bateria Automotiva",
+    descricao: "Bateria Moura 60Ah",
+    link: "https://www.heliar.com.br/produtos/detalhes-do-produto/h45je"
+}];
